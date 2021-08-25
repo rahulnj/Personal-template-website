430fa3a91a3cbad14de335fb928d6f960719ff88
Rahul <rahulkvr7a.rnj@gmail.com>
2021-08-25 22:52:23 +0530

    validationf

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -1,30 +1,138 @@
 var submitname=false,submitemail=false,submitphone=false,submitmessage=false;
-
+var mailRegx=/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
 $(document).ready(function(){
 $("#name").on('input',function(){
     this.value=this.value.replace(/[^ a-zA-Z]/, '');
     var name=$(this).val()
     if(name.length<3 || name.includes('  ')||name.charAt(0)==' '){
         $("#error-name").html("Invalid Name");
-          var submitname=false;
+           submitname=false;
     }else{
+         submitname=true;
         $("#error-name").html(" ");
-        var submitname=true;
+       
     }
 })
+
+$('#email').blur( function(){
+    var email=$(this).val()
+    if(!email.match(mailRegx)){
+        $("#error-email").html("Invalid mail");
+         submitemail=false;
+    }else{
+         submitemail=true;
+        $("#error-email").html(" ");
+       
+    }
+})
+
 
 $('#phone').on('input' ,function(){
     this.value=this.value.replace(/[^0-9]/, '').replace(/(\..*)\./, '$1');
     var phone=$(this).val()
     if(phone.length <10){
+         submitphone=false;
     $("#error-phone").html("Invalid Number");
-    var submitphone=false;
+   
     }else{
+         submitphone=true;
         $("#error-phone").html(" ");
-        var submitphone=true;
+       
     }
 })
 
-
+$('#message').blur(function(){
+    var message=$(this).val()
+    if (message.length<5){
+        console.log(submitmessage);
+        $("#error-mes").html("Please enter more than 5 characters");
+         submitmessage=false;
+    }else{
+        $("#error-mes").html(" ");
+         submitmessage=true;
+        console.log(submitname,submitemail,submitphone,submitmessage);
+    }
+})
 
 })
+
+    
+function mail() {
+    console.log(submitname,submitemail,submitphone,submitmessage);
+    if(submitname==true && submitphone==true && submitemail==true && submitmessage==true)
+    {$.ajax({
+         url:"https://script.google.com/macros/s/AKfycbw79b_crwItgWTVdvNaUylzUKAdVMOJcla-pHZm/exec",
+         data:$("#submit-form").serialize(),
+         method:"post",
+         success:function (response){
+             alert("Form submitted successfully")
+             window.location.reload()
+             //window.location.href="https://google.com"
+         },
+         error:function (err){
+             alert("Something Error")
+
+         }
+     })} else{
+
+    
+        $("#submit-message").html("Fill the Fields");
+        
+
+     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $('#submit-form').submit((e)=>{
+// e.preventDefault()
+
+
+//     if(submitname==true && submitphone==true && submitmail==true && submitmessage==true)
+//     {$.ajax({
+//          url:"https://script.google.com/macros/s/AKfycbw79b_crwItgWTVdvNaUylzUKAdVMOJcla-pHZm/exec",
+//          data:$("#submit-form").serialize(),
+//          method:"post",
+//          success:function (response){
+//              alert("Form submitted successfully")
+//              window.location.reload()
+//              //window.location.href="https://google.com"
+//          },
+//          error:function (err){
+//              alert("Something Error")
+
+//          }
+//      })
+//     } else{
+//         $("#error-name").html("Please Enter this field");
+//         $("#error-email").html("Please Enter this field");
+//         $("#error-phone").html("Please Enter this field");
+//         $("#error-mes").html("Please Enter this field");
+
+//      }
+
+// })
